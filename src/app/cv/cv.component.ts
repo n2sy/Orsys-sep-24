9dc114796718728c012309769fa24cada46b34d0
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-09-17 15:17:10 +0100

    add a model

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrl: './cv.component.css'
+  styleUrl: './cv.component.css',
 })
 export class CvComponent {
-
+  tabCandidats: Candidat[] = [
+    new Candidat(1, 'bart', 'simpson', 25, 'ingénieur', 'bart.jpeg'),
+    new Candidat(2, 'homer', 'simpson', 57, 'directeur', 'homer.png'),
+    new Candidat(3, 'lisa', 'simpson', 21, 'designer', 'lisa.png'),
+  ];
 }
