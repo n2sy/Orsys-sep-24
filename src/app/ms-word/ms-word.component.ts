05a886e9a155ad6667413db705c4ab00a1974d60
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-09-18 10:17:58 +0100

    add ms-word exercice

--- src/app/ms-word/ms-word.component.ts
+++ src/app/ms-word/ms-word.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ms-word',
   templateUrl: './ms-word.component.html',
-  styleUrl: './ms-word.component.css'
+  styleUrl: './ms-word.component.css',
 })
 export class MsWordComponent {
+  listFonts = ['', 'Arial', 'Phosphate', 'Garamond'];
+  bgColor;
+  txtColor;
+  txtSize;
+  txtFont;
 
+  changeSize(newSize) {
+    let unite = prompt('Veuillez saisir une unité');
+    this.txtSize = `${newSize}${unite}`;
+  }
 }
