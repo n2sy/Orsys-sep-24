19a6667ec9a58375b9401dc6000854f9e2ee63b1
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-09-19 10:27:03 +0100

    add navigation buttons

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.css'
+  styleUrl: './accueil.component.css',
 })
 export class AccueilComponent {
+  constructor(private router: Router) {}
 
+  goToCv() {
+    //traiteement
+    this.router.navigateByUrl('cv');
+  }
+
+  goToServers() {
+    this.router.navigate(['servers']);
+  }
 }
